84c54033a135b85600a33a7b8486af98ff65d0ed
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-11-06 14:50:10 +0530

    added stick note

--- tools.js
+++ tools.js
@@ -1,12 +1,18 @@
+let toolsCont = document.querySelector(".tools-cont");
 let optionsCont = document.querySelector(".options-cont");
 let optionsFlag = true;
-let toolsCont = document.querySelector(".tools-cont");
 let pencilToolCont = document.querySelector(".pencil-tool-cont");
 let eraserToolCont = document.querySelector(".eraser-tool-cont");
+let pencil = document.querySelector(".pencil");
+let pencilFlag = false;
+let eraser = document.querySelector(".eraser");
+let eraserFlag = false;
 
-//true  -> tools show
-//false -> hide tools
+let sticky = document.querySelector(".sticky");
+
 optionsCont.addEventListener("click", (e) => {
+	//true  -> tools show
+	//false -> hide tools
 	optionsFlag = !optionsFlag;
 
 	if (optionsFlag) openTools();
@@ -28,3 +34,34 @@ function closeTools() {
 	pencilToolCont.style.display = "none";
 	eraserToolCont.style.display = "none";
 }
+
+pencil.addEventListener("click", (e) => {
+	//    true -> show pencil tool , false -> hide pencil tool
+	pencilFlag = !pencilFlag;
+	if (pencilFlag) pencilToolCont.style.display = "block";
+	else pencilToolCont.style.display = "none";
+});
+
+eraser.addEventListener("click", (e) => {
+	// true -> show eraser tool, false -> hide eraser tool
+	eraserFlag = !eraserFlag;
+
+	if (eraserFlag) eraserToolCont.style.display = "flex";
+	else eraserToolCont.style.display = "none";
+});
+
+sticky.addEventListener("click", (e) => {
+	let stickCont = document.createElement("div");
+	stickCont.setAttribute("class", "sticky-cont");
+
+	stickCont.innerHTML = `
+	<div class="header-cont">
+		<div class="minimize"></div>
+		<div class="remove"></div>
+    </div>
+	<div class="note-cont">
+		<textarea></textarea>
+	</div>`;
+
+	document.body.appendChild(stickCont);
+});
