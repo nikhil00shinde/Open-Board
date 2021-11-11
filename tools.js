5a83c515e5b90fb3ecf3cbfb7c3bbd879abbc195
Nikhil Shinde <nikhilshinde00@outlook.com>
2021-11-11 13:00:45 +0530

    upload img and stickyCont dragndrop complete

--- tools.js
+++ tools.js
@@ -7,6 +7,7 @@ let pencil = document.querySelector(".pencil");
 let pencilFlag = false;
 let eraser = document.querySelector(".eraser");
 let eraserFlag = false;
+let upload = document.querySelector(".upload");
 
 let sticky = document.querySelector(".sticky");
 
@@ -50,18 +51,108 @@ eraser.addEventListener("click", (e) => {
 	else eraserToolCont.style.display = "none";
 });
 
-sticky.addEventListener("click", (e) => {
-	let stickCont = document.createElement("div");
-	stickCont.setAttribute("class", "sticky-cont");
+upload.addEventListener("click", (e) => {
+	//OPen file explorer
+	let input = document.createElement("input");
+	input.setAttribute("type", "file");
+	input.click();
 
-	stickCont.innerHTML = `
+	input.addEventListener("change", (e) => {
+		let file = input.files[0];
+		let url = URL.createObjectURL(file);
+		let stickyTemplateHTML = `
+		<div class="header-cont">
+			<div class="minimize"></div>
+			<div class="remove"></div>
+		</div>
+		<div class="note-cont">
+           <img src="${url}"/>
+		</div>`;
+
+		createSticky(stickyTemplateHTML);
+	});
+});
+
+// adding sticky container
+sticky.addEventListener("click", (e) => {
+	let stickyTemplateHTML = `
 	<div class="header-cont">
 		<div class="minimize"></div>
 		<div class="remove"></div>
     </div>
 	<div class="note-cont">
-		<textarea></textarea>
+		<textarea spellcheck="false></textarea>
 	</div>`;
 
-	document.body.appendChild(stickCont);
+	createSticky(stickyTemplateHTML);
 });
+
+//creating sticky container
+function createSticky(stickyTemplateHTML) {
+	let stickyCont = document.createElement("div");
+	stickyCont.setAttribute("class", "sticky-cont");
+
+	stickyCont.innerHTML = stickyTemplateHTML;
+	document.body.appendChild(stickyCont);
+
+	let minimize = stickyCont.querySelector(".minimize");
+	let remove = stickyCont.querySelector(".remove");
+
+	noteActions(minimize, remove, stickyCont);
+
+	stickyCont.onmousedown = function (event) {
+		dragAndDrop(stickyCont, event);
+	};
+
+	stickyCont.ondragstart = function () {
+		return false;
+	};
+}
+
+//removing and minimizing sticky container
+function noteActions(minimize, remove, stickyCont) {
+	remove.addEventListener("click", (e) => {
+		stickyCont.remove();
+	});
+
+	minimize.addEventListener("click", (e) => {
+		let noteCont = stickyCont.querySelector(".note-cont");
+		let display = getComputedStyle(noteCont).getPropertyValue("display");
+		if (display === "none") {
+			noteCont.style.display = "block";
+		} else {
+			noteCont.style.display = "none";
+		}
+	});
+}
+
+//drag n drop function on sticky container
+function dragAndDrop(element, event) {
+	let shiftX = event.clientX - element.getBoundingClientRect().left;
+	let shiftY = event.clientY - element.getBoundingClientRect().top;
+
+	element.style.position = "absolute";
+	element.style.zIndex = 1000;
+
+	moveAt(event.pageX, event.pageY);
+
+	// moves the element at (pageX, pageY) coordinates
+	// taking initial shifts into account
+	function moveAt(pageX, pageY) {
+		element.style.left = pageX - shiftX + "px";
+		element.style.top = pageY - shiftY + "px";
+	}
+
+	function onMouseMove(event) {
+		moveAt(event.pageX, event.pageY);
+	}
+
+	// move the element on mousemove
+	document.addEventListener("mousemove", onMouseMove);
+
+	// drop the element, remove unneeded handlers
+	element.onmouseup = function () {
+		document.removeEventListener("mousemove", onMouseMove);
+		element.onmouseup = null;
+	};
+}
